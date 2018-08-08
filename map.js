41f3a1073788f8f9f9b57526306b4fa9afa8e3ed
fifi9573 <fifi9573@gmail.com>
2018-08-08 09:15:48 -0400

    Adding map and buttons

--- map.js
+++ map.js
@@ -27,13 +27,67 @@ function init() {
         view:view
     });
 
-    console.log("LOADED@");
+    console.log("we loaded this init function BAM");
 }
 
-// How do we know our web page loads for the first time?
+// AFTER SHE STRUGGLED FOR 80 YEARS, Kanyce finally realized, 
+// Part of the DOM pattern (style of coding)
+// console.log == print for JS, which prints *gag
+// To CREATE the map, we RUN our init function
+// This runs when we first open up our website
+
 // document.addEventListener("DOMContentLoaded", function (e){
-//     console.log("LOADED LSKDMFLKSDMFLKSDMFLKM")
-//     init()
+//     console.log("LOADED LSKDMFLKSDMFLKSDMFLKM");
+//     init();
 // });
 
 window.onload = init;
+
+
+// Animates the map - zooms in (but we need to connect this to a button)
+function panHome() {
+    view.animate({
+        center: ourLoc,
+        duration: 2000
+    });
+}
+
+// Create a function that will go to a specific location
+// Using another API
+
+function panToLocation() {
+    var countryName = document.getElementById("country-name").value;
+
+    //Error check to make sure the country is spelled correctly
+    if(countryName === "United States") {
+        alert("You didn't enter a country name!");
+        return;
+    }
+
+    //we're accessing a REST API to get the country's 
+    //location data (we customize the query to access the API)
+    var query = "https://restcountries.eu/rest/v2/name/"+countryName;
+    query = query.replace(/ /g, "%20");
+    alert(query);
+
+    // We are now REQUESTING - talking to the server
+    var countryRequest = new XMLHttpRequest();
+    countryRequest.open('GET', query, false);
+
+    // WE have a request, send it to the server 
+    countryRequest.send();
+    var lat = countryInformation[0].latlng[0];
+    var lon = countryInformation[0].lonlng[0];
+    
+    
+
+    var location = ol.proj.fromLonLat([lon, lat]);
+
+    view.animate({
+        center: location,
+        duration: 2000
+    });
+    
+}
+
+
